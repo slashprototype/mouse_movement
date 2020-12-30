21c4575ec227354083ad88d6d997b9b0ce5faf6c
Guillermo[1;5C[1;5A[1;5D[1;5C[1;5D <gameropixel@gmail.com>
2020-12-30 13:46:10 -0600

    New readme and mouse.py code reduction

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# mouse_movement
-This is a python script that allows you controlling your mouse with arrow keys in special situations
+# The mouse movement project
+_This is a python script that allows you controlling your mouse with arrow keys in special situations_
+# Instructions
+
+_Follow this instructions to know how to use this script_
+
+```
+Combo keywords:
+"ctrl+
+```
+
+
+
--- mouse_movement.py
+++ mouse_movement.py
@@ -6,6 +6,7 @@ import time
 class Mouse():
     activated = False
     speed = 3
+
     def move(self, direction):
         flags, hcursor, (x,y) = win32gui.GetCursorInfo()
         if direction == 'up':
@@ -22,34 +23,29 @@ class Mouse():
     def click(self, side):
         if side == "left":
             win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-            time.sleep(0.1)
+            time.sleep(0.5)
             win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 #Main program
 m1 = Mouse()
 key = ('up','down','left','right')
 
-while not keyboard.is_pressed('ctrl+shift'):
+while not keyboard.is_pressed('ctrl+shift+mayusc'):
     
     if keyboard.is_pressed('ctrl+alt'):
         m1.activated=   not m1.activated
-        print(m1.activated)
+        print('keyboard script is: '+ str(m1.activated))
         time.sleep(0.5)
 
-
-
-
     if m1.activated:
-        if keyboard.is_pressed('up'):
-            m1.move('up')
-        if keyboard.is_pressed('down'):
-            m1.move('down')
-        if keyboard.is_pressed('left'):
-            m1.move('left')
-        if keyboard.is_pressed('right'):
-            m1.move('right')
-        if keyboard.is_pressed('shift'):
-            m1.click('left')
-        time.sleep(0.01)
+        for x in key:
+
+            if keyboard.is_pressed(x):
+                m1.move(x)
+
+            if keyboard.is_pressed('shift'):
+                m1.click('left')
+
+            time.sleep(0.01)
 
  
